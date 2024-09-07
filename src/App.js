299b7f043f90da358f2b08527d66e44c27099990
lubuntum <lubuntum@gmail.com>
2024-09-07 12:02:03 +0700

    Обновленная точка входа  снавигацией и авторизацией

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
+import Login from './components/auth/Login';
 import './App.css';
-
+import AuthProvider, { useAuth } from './modules/auth/AuthProvider';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ProtectedRoute from './modules/auth/ProtectedRoute';
+import AccountInfo from './components/account/AccountInfo';
+import MainPage from './components/MainPage';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AuthProvider>
+        <Main/>
+      </AuthProvider>
+    </BrowserRouter>
   );
+}
+
+const Main = () => {
+  return (
+    <>
+    <Routes>
+      <Route path='*' element = {<Login/>} />
+      <Route path='/account' element = {<ProtectedRoute component={<AccountInfo/>}/>}/>
+      <Route path='/home' element = {<ProtectedRoute component={<MainPage/>}/>} />
+    </Routes>
+    </>
+  )
 }
 
 export default App;
