a1189379e69d81679eeba7c162becb9ccace7aa9
lubuntum <lubuntum@gmail.com>
2024-09-27 16:09:50 +0700

    Вынесли логику работы хедеров для авторизованных и не авторизованных пользователей

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,20 +1,16 @@
 import { useAuth } from "../../modules/auth/AuthProvider"
+import UnAuthHeader from "./UnAuthHeader"
+import AuthHeader from "./AuthHeader"
 import "./header.css"
 const Header = () => {
     const {logout, isAuth, getUsername} = useAuth()
 
     return(
-        <div className="header-wrap">
-            <div className="header-container">
-                <div className="logo-container">
-                    <i className="fas fa-thumbs-up"></i>
-                </div>
-                <div className="options">
-                    {(isAuth && <div className="option">{getUsername()}</div>)}
-                    {(isAuth && <div><a href="" onClick={()=>{logout()}}>Выход</a></div>)}
-                </div>
-            </div>
-        </div>
+        <>
+            {(isAuth && <AuthHeader/>)}
+            {(!isAuth && <UnAuthHeader/>)}
+        </>
+        
     )
 }
 
