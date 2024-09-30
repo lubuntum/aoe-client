a972ee1b555612ab2d116311688e1eb8ab03fbd6
lubuntum <lubuntum@gmail.com>
2024-09-30 14:12:12 +0700

    Роуты для навигации

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+const REGISTER = "/register";
+const LOGIN = "/login";
+const HOME = "/home";
+const ACCOUNT = "/account";
+const routes = {
+    REGISTER,
+    LOGIN,
+    HOME,
+    ACCOUNT,
+};
+export default routes
