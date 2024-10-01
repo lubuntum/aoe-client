0ee67b14583209ad356f9e472b8acf882294969e
A1tBot <timofeyershovv@gmail.com>
2024-10-01 17:27:35 +0700

    Убрал лишние пробелы

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,14 +3,12 @@ import Header from './header/Header';
 
 const HomePage = () => {
     const {isAuth, getUsername} = useAuth()
-    return (
-        <>
-            <Header/>
-            {isAuth && <p> Привет {getUsername()}</p>}
-            {!isAuth && <p> Гость </p>}
-            <p>Main content</p>
-        </>
-    )
+    return (<>
+        <Header/>
+        {isAuth && <p> Привет {getUsername()}</p>}
+        {!isAuth && <p> Гость </p>}
+        <p>Main content</p>
+    </>)
 }
 
 export default HomePage
