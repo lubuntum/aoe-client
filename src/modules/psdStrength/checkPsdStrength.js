85ec2aa5fd4dec1b474b1a120f82f03f018d7cbb
A1tBot <timofeyershovv@gmail.com>
2024-10-01 19:14:59 +0700

    Доработали функцию обработки силы пароля (надо наверно допилить как нить)

--- src/modules/psdStrength/checkPsdStrength.js
+++ src/modules/psdStrength/checkPsdStrength.js
@@ -1,7 +1,17 @@
-//red
-//orange
-//green
-
+const BAD_PASS = "badPassStyle";
+const MIDDLE_PASS = "middlePassStyle"
+const GOOD_PASS = "goodPassStyle"
 export const checkPsdStrength = (pass) => {
-    return 'some style'
+    let strength = 0;
+    if (pass.length >= 8) strength += 5;
+    if(/[a-z]/.test(pass)) strength += 1;
+    if(/[A-Z]{2}/.test(pass)) strength += 1;
+    if(/[0-9]/.test(pass)) strength += 1;
+    if(/[\W_]/.test(pass)) strength += 1;//Спец символы
+    strength = (strength / 9) * 100 //0 - 100%
+    if (strength <= 61.0)
+         return BAD_PASS
+    else if (strength > 61.0 && strength <= 70.0)
+        return MIDDLE_PASS
+    return GOOD_PASS
 }
