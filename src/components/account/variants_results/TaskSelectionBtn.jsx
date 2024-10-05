dd3ebf644bfe1c7271f21e12af1e69b5c0bbbda3
A1tBot <timofeyershovv@gmail.com>
2024-10-05 10:53:10 +0700

    Микроизменения

--- src/components/account/variants_results/TaskSelectionBtn.jsx
+++ src/components/account/variants_results/TaskSelectionBtn.jsx
@@ -4,8 +4,8 @@ export const TaskSelectionBtn = ({type, id, task, showContentByTaskClick}) => {
     return(<>
         <div className="buttonContainer">
             <fieldset id="tasksGroup" className="radioContainer">
-                <input type="radio" id={type+id} name="tasksGroup"></input>
-                <label for={type+id} onClick={()=>{showContentByTaskClick(task)}}><p>Задание {id+1}</p><span style={{display: "none"}}>{id+1}</span></label>
+                <input type="radio" id={type+(id+1)} name="tasksGroup"></input>
+                <label for={type+(id+1)} onClick={()=>{showContentByTaskClick(task)}}><p>Задание {id+1}</p><span style={{display: "none"}}>{id+1}</span></label>
             </fieldset>
         </div>
     </>)
