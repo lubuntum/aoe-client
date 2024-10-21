d81474c6dc15fcdea34baa485f92f9118537175b
lubuntum <lubuntum@gmail.com>
2024-10-21 17:47:05 +0700

    Добавил базовую логику для компонента MicroPerfomance

--- src/components/lesson_sesson/LessonSessionPage.jsx
+++ src/components/lesson_sesson/LessonSessionPage.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react"
 import Header from "../header/Header"
-
+import { MicroPerfomance } from "./MicroPerfomance"
+import "./css/lesson.css"
+import "./css/micro_perfomance.css"
 export const LessonSessionPage = ({variant}) => {
+    const [microCheck, setMicroCheck] = useState(false)
     return (
         <>
-        <Header/>
+        <div className="lessonWrapper">
+            <Header/>
+            {!microCheck && <MicroPerfomance/>}
+        </div>
+        
         </>
     )
 }
