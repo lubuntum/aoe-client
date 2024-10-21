2a9f035003dd0ce4d8ffb614a6b294b295c65648
lubuntum <lubuntum@gmail.com>
2024-10-21 17:48:21 +0700

    Сам компонент для проверки микрофона (некоторые стили инлайн, вынести!)

---
+++ src/components/lesson_sesson/MicroPerfomance.jsx
@@ -0,0 +1,24 @@
+import useMediaRecorder from "../../hooks/useMediaRecorder"
+
+export const MicroPerfomance = () => {
+    const {mediaBlobUrl, isRecording, startRecording, stopRecording} = useMediaRecorder(true)
+    return (
+        <>
+            <div className="checkMicroContainer">
+                <p>Micro perfomance check before the exam</p>
+                {isRecording ? <a className="btn" style={{width:'auto',padding:'0px 15px', background:'red'}} onClick={() => stopRecording()} >Стоп</a>
+                    : <a className="btn" style={{width:'auto',padding:'0px 15px'}} onClick={() => startRecording()} >Проверить</a>
+                }
+                {mediaBlobUrl &&
+                    <audio controls src={mediaBlobUrl} >
+                        Ваш браузер не поддерживает audio тег
+                    </audio>}
+            </div>
+            
+            <div style={{display:'flex', justifyContent:'center'}}>
+                <a className="btn" style={{width:'auto',padding:'0px 15px'}} >Преступить</a>
+            </div>
+            
+        </>
+    )
+}
