8ecd687af4bddd3f4480935787d822dd986d1bc4
lubuntum <lubuntum@gmail.com>
2024-10-24 02:46:58 +0700

    Сделал пока переключение контента по клику, НО нужно скорее всего сделать отдельные компоненты с последовательной озвучкой и своей бизнес логикой, поскольку эти только для показа контента...

--- src/components/lesson_sesson/LessonSessionPage.jsx
+++ src/components/lesson_sesson/LessonSessionPage.jsx
@@ -5,8 +5,10 @@ import "./css/lesson.css"
 import "./css/micro_perfomance.css"
 import { useLocation } from "react-router-dom"
 import {getTasksByVariantId} from "../../modules/api/variant/VariantApi"
+import { TaskContentViewer } from "../account/variants_results/content_viewer/TaskContentViewer"
 export const LessonSessionPage = () => {
     const [tasks, setTasks] = useState([])
+    const [currentTaskType, setCurrentTaskType] = useState(1)
     const location = useLocation()
     const variant = location.state || {}
     const [microCheck, setMicroCheck] = useState(false)
@@ -23,6 +25,11 @@ export const LessonSessionPage = () => {
         loadTasksByVariantId()
     }, [])
 
+    const handleNextTask = () => {
+        if (currentTaskType >= 4 || currentTaskType <= 0) return;
+        setCurrentTaskType(currentTaskType+1)
+    }
+
     return (
         <>
         <div className="lessonWrapper">
@@ -31,9 +38,18 @@ export const LessonSessionPage = () => {
             (<>
                 <MicroPerfomance/>
                 <div style={{display:'flex', justifyContent:'center'}}>
-                    <a className="btn" style={{width:'auto',padding:'0px 15px'}} >Приступить</a>
+                    <a onClick={()=> {setMicroCheck(true)}} className="btn" style={{width:'auto',padding:'0px 15px'}} >Приступить</a>
                 </div>
             </>)
+            }
+            {microCheck && 
+            (<>
+                <TaskContentViewer task={tasks.find((t)=>t.taskType === currentTaskType)}/>
+                <div style={{display:'flex', justifyContent:'center'}}>
+                    <a onClick={()=> {handleNextTask()}} className="btn" style={{width:'auto',padding:'0px 15px'}} >Далее</a>
+                </div>
+            </>)
+                
             }
         </div>
         </>
