acdce10a5ee2adb406ec52ceb5fb3bcf057b29e5
lubuntum <lubuntum@gmail.com>
2024-10-23 11:02:46 +0700

    Новый метод для загрузки заданий по id варианта

--- src/modules/api/variant/VariantApi.js
+++ src/modules/api/variant/VariantApi.js
@@ -1,7 +1,13 @@
 import axios from "axios"
-import { API_VARIANTS_DATA, SERVER_API_URL } from "../../../config";
+import { API_VARIANT_TASKS_DATA, API_VARIANTS_DATA, SERVER_API_URL } from "../../../config";
 
 export const getVariantsData = async () => {
     const response = await axios.get(`${SERVER_API_URL}${API_VARIANTS_DATA}`);
+    return response;
+}
+
+export const getTasksByVariantId = async (variantId) => {
+    const url = API_VARIANT_TASKS_DATA.replace("%d", variantId)
+    const response = await axios.get(`${SERVER_API_URL}${url}`);
     return response;
 }
