2c800fca2189af8b6d1a611fc80b33136feaf7c6
lubuntum <lubuntum@gmail.com>
2024-10-23 11:03:55 +0700

    Загрузка заданий с сервера + использование location для получения variant

--- src/components/lesson_sesson/LessonSessionPage.jsx
+++ src/components/lesson_sesson/LessonSessionPage.jsx
@@ -1,17 +1,30 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Header from "../header/Header"
 import { MicroPerfomance } from "./MicroPerfomance"
 import "./css/lesson.css"
 import "./css/micro_perfomance.css"
-export const LessonSessionPage = ({variant}) => {
+import { useLocation } from "react-router-dom"
+import {getTasksByVariantId} from "../../modules/api/variant/VariantApi"
+export const LessonSessionPage = () => {
+    const [tasks, setTasks] = useState([])
+    const location = useLocation()
+    const variant = location.state || {}
     const [microCheck, setMicroCheck] = useState(false)
+
+    useEffect(()=>{
+        const loadTasksByVariantId = async () => {
+            const response = await getTasksByVariantId(variant.id)
+            setTasks(response.data)
+        }
+        loadTasksByVariantId()
+    }, [])
+
     return (
         <>
         <div className="lessonWrapper">
             <Header/>
             {!microCheck && <MicroPerfomance/>}
         </div>
-        
         </>
     )
 }
