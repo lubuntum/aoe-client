5169b85848ff591f5d67211fec95de567c1f193e
lubuntum <lubuntum@gmail.com>
2024-11-06 03:44:00 +0700

    запись ответа в четвертом задании

--- src/components/lesson_sesson/task_session/FourthTaskSession.jsx
+++ src/components/lesson_sesson/task_session/FourthTaskSession.jsx
@@ -1,9 +1,16 @@
+import useLessonMediaRecorder from "../../../hooks/useLessonMediaRecorder"
 import { TaskContentViewer } from "../../account/variants_results/content_viewer/TaskContentViewer"
 import { stages } from "../LessonSessionPage"
 import { TaskSessionPanel } from "./TaskSessionPanel"
 
 export const FourthTaskSession = ({task, stage, setStage, handleNextTask}) => {
+    const {audioBlobRef, startRecording, stopRecording} = useLessonMediaRecorder(true)
+    const handleNextTaskWithSaveAudio = async () => {
+        await stopRecording()
+        handleNextTask({"audio": audioBlobRef.current, "taskId": task.id})
+    }
     console.log("Forth component")
+    if(stage === stages.speak) startRecording()
     return (
         <>
             <TaskContentViewer task={task} />
@@ -11,7 +18,7 @@ export const FourthTaskSession = ({task, stage, setStage, handleNextTask}) => {
                 <TaskSessionPanel nextAction={()=>{setStage(stages.prepare_speak)}} btnText={"Skip"} sec={5} />
             }
             {stage === stages.speak &&
-                <TaskSessionPanel nextAction={()=>{handleNextTask()}} btnText={"Next"} sec={5} />
+                <TaskSessionPanel nextAction={()=>{handleNextTaskWithSaveAudio()}} btnText={"Next"} sec={5} />
             }
         </>
     )
