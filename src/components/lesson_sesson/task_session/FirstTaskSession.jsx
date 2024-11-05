4e03cff11535f57e054400ed6c1e98558dd65938
lubuntum <lubuntum@gmail.com>
2024-11-06 03:42:40 +0700

    Добавлена возможность записи первого задания

--- src/components/lesson_sesson/task_session/FirstTaskSession.jsx
+++ src/components/lesson_sesson/task_session/FirstTaskSession.jsx
@@ -7,20 +7,30 @@
  * task.taskContent (str obj)
  * @returns 
  */
+import { useEffect } from "react"
+import useMediaRecorder from "../../../hooks/useMediaRecorder"
 import { TaskContentViewer } from "../../account/variants_results/content_viewer/TaskContentViewer"
 import {stages} from "../LessonSessionPage"
 import { TaskSessionPanel } from "./TaskSessionPanel"
+import useLessonMediaRecorder from "../../../hooks/useLessonMediaRecorder"
 export const FirstTaskSession = ({task, stage, setStage, handleNextTask}) => { //blobRef + micro hook
-
-    console.log(task)
+    const {audioBlobRef, startRecording, stopRecording} = useLessonMediaRecorder(true)
+    const handleNextTaskWithSaveAudio =  async () => {
+        await stopRecording()
+        console.log({"audio": audioBlobRef.current, "taskId": task.id})
+        handleNextTask({"audio": audioBlobRef.current, "taskId": task.id})
+        
+    }
+    if (stage === stages.speak) startRecording();
+    console.log('first task render')
     return (
         <>
             {(stage === stages.reading || stage === stages.speak) && <TaskContentViewer task={task} />}
             {stage === stages.reading && 
                 <TaskSessionPanel btnText={"Skip"} nextAction={()=> {setStage(stages.prepare_speak)}} sec={90} />}
             {stage === stages.speak && 
-                <TaskSessionPanel btnText={"Next"} nextAction={()=> {handleNextTask()}} sec={90} />
-                }
+                <TaskSessionPanel btnText={"Next"} nextAction={()=> {handleNextTaskWithSaveAudio()}} sec={90} />
+            }
         </>
     )
 }
