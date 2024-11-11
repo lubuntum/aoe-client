4ce4dc5e543472a37e80a5237b1fbca6a5470764
lubuntum <lubuntum@gmail.com>
2024-11-11 14:39:50 +0700

    Обновил роуты

--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,15 @@ const HOME = "/home";
 const ACCOUNT = "/account";
 const TASK = "/tasks";
 const LESSON_SESSION = "/session"
+const RESULTS = "/results"
 const routes = {
     REGISTER,
     LOGIN,
     HOME,
     ACCOUNT,
     TASK,
-    LESSON_SESSION
+    LESSON_SESSION,
+    RESULTS
 };
 
 export default routes
