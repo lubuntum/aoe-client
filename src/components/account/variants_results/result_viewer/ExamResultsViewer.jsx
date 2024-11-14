a7b117745f59a4e81aa22fc5bc75a154ae6712aa
lubuntum <lubuntum@gmail.com>
2024-11-14 13:56:38 +0700

    Передал вариант для отображения темы в панели (нужно создать обертки для панели тасков и панели экзамена и для этого вынести UI панельки в новый копонент )

--- src/components/account/variants_results/result_viewer/ExamResultsViewer.jsx
+++ src/components/account/variants_results/result_viewer/ExamResultsViewer.jsx
@@ -10,7 +10,7 @@ import { ReactComponent as DownloadIcon } from "../../../../res/icons/download_2
 import { ReactComponent as ProtocolIcon } from "../../../../res/icons/receipt_long_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg"
 import { ReactComponent as ExpandIcon } from "../../../../res/icons/quick_reference_all_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg"
 
-export const ExamResultsViewer = () => {
+export const ExamResultsViewer = ({variant}) => {
     const data = Array.from({length: 19}, (_, index) => ({
         id: index + 1,
         complete: getCurrentDate(),
@@ -56,7 +56,7 @@ export const ExamResultsViewer = () => {
         <div className="resultsViewerContainer gridItem9">
             <div className="resultsViewerDescription">
                 <p>Ваши попытки</p>
-                <p>Ecology And Ecology</p>
+                <p>{variant ? variant.theme : "Тема не найдена"}</p>
                 <p>Экзамен</p>
             </div>
             
