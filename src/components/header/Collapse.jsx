4a33e882d81a9d3d39987d27624142c7afe9142f
A1tBot <timofeyershovv@gmail.com>
2024-11-23 00:48:18 +0700

    Aboba (Дополнение к предыдущему комиту)

--- src/components/header/Collapse.jsx
+++ src/components/header/Collapse.jsx
@@ -1,11 +1,31 @@
 import { ReactComponent as MenuIcon } from "../../res/icons/menu_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg"
 
-export const Collapse = ({burgerTop}) => {
+import { useEffect, useState } from "react"
+
+export const Collapse = () => {
+
+    const [collapseTop, setCollapseTop] = useState(false);
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 20) {
+                setCollapseTop(true);
+            } else {
+                setCollapseTop(false);
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, [])
+
     return (<>
         <div className="collapseMenu">
             <input type="checkbox" id="collapseMenuCheckbox"></input>
             <label className="btn" for="collapseMenuCheckbox"><MenuIcon className="svgIcon"/> Меню</label>
-            <nav className={`${burgerTop ? 'collapseTop120' : 'collapseTop140'}`}>
+            <nav className={`${collapseTop ? 'collapseTop120' : 'collapseTop140'}`}>
                 <a href="#">Преимущества</a>
                 <a href="#">Как начать учиться</a>
                 <a href="#">Отзывы</a>
