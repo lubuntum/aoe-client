d61c5e0731851197878eae02bfa72a304f2d158e
lubuntum <lubuntum@gmail.com>
2024-12-06 00:17:51 +0700

    Пофиксил побочные функции, неправильно обрабатывали if guard

--- src/modules/gradeFormat/setGradeColor.js
+++ src/modules/gradeFormat/setGradeColor.js
@@ -2,9 +2,12 @@ const GRADE_RED = "gradeRed";
 const GRADE_YELLOW = "gradeYellow"
 const GRADE_GREEN = "gradeGreen"
 export const setGradeColor = (grade, total) => {
-    if (!grade) return ""
+    if (grade === null) return ""
     const percentage = (grade / total) * 100
+    console.log(`percentage = ${percentage}`)
     if (percentage < 69) return GRADE_RED
     if (percentage >= 70 && percentage <= 84) return GRADE_YELLOW
     return GRADE_GREEN
 }
+//TODO Тима сделай что бы по дефолту возвращал черный цвет, а не зеленый
+//что бы символ "--" был черный
--- src/modules/gradeFormat/setGradeFormat.js
+++ src/modules/gradeFormat/setGradeFormat.js
@@ -1,4 +1,4 @@
 export const setGradeFormat = (grade) => {
-    if(!grade) return "--"
+    if(grade == null) return "--"
     return grade.toString().padStart(2, "0")
 }
