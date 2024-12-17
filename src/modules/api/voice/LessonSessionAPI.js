22a8658d41ca1266117289775c54481e1a11ddae
lubuntum <lubuntum@gmail.com>
2024-12-17 19:25:18 +0700

    Пофиксил наименование файла при отправке

--- src/modules/api/voice/LessonSessionAPI.js
+++ src/modules/api/voice/LessonSessionAPI.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { API_SEND_EXAM_DATA, API_SEND_TASK_RESULT_DATA, API_SEND_USER_TASK_DATA, SERVER_API_URL, USER_NAME } from "../../../config";
+import { API_SEND_EXAM_DATA, API_SEND_TASK_RESULT_DATA, API_SEND_USER_TASK_DATA, SERVER_API_URL, USER_EMAIL, USER_NAME } from "../../../config";
 import { getCurrentDate } from "../../date/currentDate";
 export const createExamRequest = async (variantId, sessionKey) => {
     const response = await axios.post(`${SERVER_API_URL}${API_SEND_EXAM_DATA}`,
@@ -13,10 +13,10 @@ export const createExamRequest = async (variantId, sessionKey) => {
  * @param {*} taskResult data with audio blob and etc
  */
 export const saveUserTaskRequest = async (examId = null, taskId, audioBlob, sessionKey) => {
-    const username = localStorage.getItem(USER_NAME)
+    const userEmail = localStorage.getItem(USER_EMAIL)
     const userTask = {"taskId":taskId, "examId":examId, "completeDate": getCurrentDate()}
     const form = new FormData()
-    form.append("file", audioBlob, `${username}_${taskId}_${Date.now()}.wav`);
+    form.append("file", audioBlob, `${userEmail}_${taskId}_${Date.now()}.wav`);
     form.append("customerTask", new Blob([JSON.stringify(userTask)], {type:"application/json"}))
     const response = await axios.post(`${SERVER_API_URL}${API_SEND_USER_TASK_DATA}`,
         form, {headers: {Authorization:sessionKey ,"Content-Type":"multipart/form-data"}, 
