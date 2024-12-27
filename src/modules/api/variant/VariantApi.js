e3b93da9244251dae7184959a7b2a9b087dfd5a2
lubuntum <lubuntum@gmail.com>
2024-12-27 12:17:44 +0700

    Расширено API для получения вариантов, а также изменения их видимости

--- src/modules/api/variant/VariantApi.js
+++ src/modules/api/variant/VariantApi.js
@@ -1,9 +1,15 @@
 import axios from "axios"
-import { API_ADMIN_DELETE_VARIANT, API_ADMIN_SEND_VARIANT, API_ADMIN_UPLOAD_TASKS, API_ADMIN_UPLOAD_VARIANT, API_VARIANT_TASKS_DATA, API_VARIANTS_DATA, SERVER_API_URL, USER_DATA_KEY } from "../../../config";
+import { API_ADMIN_DELETE_VARIANT, API_ADMIN_SEND_VARIANT, API_ADMIN_UPLOAD_TASKS, API_ADMIN_UPLOAD_VARIANT, API_ADMIN_VARIANT_VISIBILITY, API_ADMIN_VARIANTS, API_VARIANT_TASKS_DATA, API_VARIANTS_DATA, SERVER_API_URL, USER_DATA_KEY } from "../../../config";
 import { getCurrentDate } from "../../date/currentDate";
 
-export const getVariantsData = async () => {
+export const getVisibleVariants = async () => {
     const response = await axios.get(`${SERVER_API_URL}${API_VARIANTS_DATA}`);
+    return response;
+}
+export const getAllVariants = async (token) => {
+    const response = await axios.get(`${SERVER_API_URL}${API_ADMIN_VARIANTS}`, {
+        headers : {"Authorization" : token}
+    })
     return response;
 }
 
@@ -15,15 +21,21 @@ export const getTasksByVariantId = async (variantId) => {
     })
     return response;
 }
+export const updateVariantVisibility = async (variantId, visibility, token) => {
+    const param = new URLSearchParams()
+    param.append("visibility", visibility)
+    const response = axios.post(`${SERVER_API_URL}${API_ADMIN_VARIANT_VISIBILITY}/${variantId}`,param ,{
+        headers:{"Authorization" : token}
+    })
+    return response;
+}
 
 export const sendVariantData = async (variant) => {
     const formData = new FormData()
     formData.append('variantImg', variant.variantImg)
     formData.append('variantName', variant.variantName)
     formData.append('creationDate', getCurrentDate())
-    formData.forEach((value, key) => {
-        console.log(key, value);
-    });
+    
     const response = await axios.post(`${SERVER_API_URL}${API_ADMIN_UPLOAD_VARIANT}`,
          formData, {headers : {Authorization : localStorage.getItem("token"),"Content-Type": 'multipart/form-data'}})
     return response
