6d12a48eb7bc0fbdf0fc28dab9e5651a7990317c
lubuntum <lubuntum@gmail.com>
2025-01-14 17:41:48 +0700

    Обновленный конфиг

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ export const USER_EMAIL = "user_email"
 
 export const GUEST_NAME = "Гость"
 //Линк сервера
-export const SERVER_API_URL = "http://localhost:8080"
+export const SERVER_API_URL = "https://192.168.85.75:8080"
 //Роуты для валидации
 export const API_REGISTRATION = "/api/auth/registration"
 export const API_LOGIN = "/api/auth/login"
@@ -41,3 +41,6 @@ export const API_ADMIN_VALIDATE = "/api/admin/validate"
 export const API_ADMIN_VARIANTS = "/api/admin/variants"
 export const API_ADMIN_TASKS_TYPES = "/api/admin/tasks-types"
 export const API_ADMIN_UPDATE_TASK_TYPE = "/api/admin/update-prompt"
+
+export const API_ADMIN_ADD_BALANCE_TO_CUSTOMER = "/api/admin/add-balance-to-customer"
+export const API_ADMIN_GET_CUSTOMER_ID_BY_EMAIL = "/api/admin/customer-id-by-email"
