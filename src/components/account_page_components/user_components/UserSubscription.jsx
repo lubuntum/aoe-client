dfd9fdd0087cfbf08a570d65cf9c16c472a10e3c
lubuntum <lubuntum@gmail.com>
2025-01-20 19:31:05 +0700

    Обновлена работа с подписками, теперь даты высчитываются нормально

--- src/components/account_page_components/user_components/UserSubscription.jsx
+++ src/components/account_page_components/user_components/UserSubscription.jsx
@@ -1,55 +1,38 @@
 import { useNavigate } from "react-router-dom"
 import routes from "../../../routes"
 
-import { subscriptionEndDate, subscriptionRemain } from "../../../modules/date_modules/subscriptionDate"
+import { subscriptionDaysRemain, subscriptionRemainPercent } from "../../../modules/date_modules/subscriptionDate"
 
 import { Button } from "../../reusible_components/Button"
 
 export const UserSubscription = ({customer, className}) => {
     const navigate = useNavigate()
-
-    let subscriptionPurchase = undefined
-    let endSubDate = undefined
-    let endSubDateStr = undefined
-    let daysRemain = undefined
-    let daysRemainPersent = undefined
-
-    if (customer.actualSubscriptionDate === undefined || customer.actualSubscriptionDate === null
-        || customer.actualSubscriptionDate === '') {
-        subscriptionPurchase = <p>Подписки нет</p>
-    } else {
-        endSubDate = subscriptionEndDate(customer.actualSubscriptionDate);
-        endSubDateStr = endSubDate.toLocaleDateString('en-GB').replace(/\//g,'.') //DD.MM.YYYY
-        daysRemain = subscriptionRemain(endSubDate)
-        //TODO не делить на 30, потому-что должно зависить от длительности подписки
-        daysRemainPersent = Math.round(daysRemain === 0 ? 0 : (daysRemain / 30) * 100)
-        //daysRemainPersent = `${(daysRemain / 30) * 100}%`
-    }
-
+    const daysRemain = customer.expireSubDate ? subscriptionDaysRemain(customer.expireSubDate) : null
+    const daysRemainPercent = (customer.purchaseSubDate && customer.expireSubDate) ? subscriptionRemainPercent( customer.purchaseSubDate, customer.expireSubDate) : null
     const setColor = (percent) => {
         if (percent > 51) {return "#06d6a0"}
         if (percent <= 50 && percent >= 26) {return "#ffd166"}
         if (percent <= 25){return "#ef476f"}
     }
-    const progressBarColor = setColor(daysRemainPersent)
+
     
     return (<>
         <div className={`userSubscriptionContainer ${className}`}>
             <p>Подписка</p>
             
-            {subscriptionPurchase === undefined ? <>
+            {(customer.expireSubDate && daysRemain >= 0) ? <>
                 <div className="userSubscriptionDateEnd">
-                    <p>Действует до: <span>{endSubDateStr}</span></p>
+                    <p>Действует до: <span>{customer.expireSubDate}</span></p>
                     <p><span>{daysRemain}</span>д.</p>
                 </div>
                 
                 <div className="userProgressBarBackground">
-                    <div className="userProgressBarLine" style={{width: `${daysRemainPersent}%`, 
-                                                                 backgroundColor: progressBarColor,
-                                                                 boxShadow: `0 0 5px ${progressBarColor}`,
+                    <div className="userProgressBarLine" style={{width: `${daysRemainPercent}%`, 
+                                                                 backgroundColor: setColor(daysRemainPercent),
+                                                                 boxShadow: `0 0 5px ${setColor(daysRemainPercent)}`,
                     }}></div>
                 </div>
-            </> : subscriptionPurchase}
+            </> : <p>Подписки нет</p>}
 
             <Button buttonType={""}
                     buttonPadding={"0 20px"}
--- src/modules/date_modules/subscriptionDate.js
+++ src/modules/date_modules/subscriptionDate.js
@@ -1,3 +1,4 @@
+//deprecated
 export const subscriptionEndDate = (startDateStr) => {
     const dateParts = startDateStr.split('.')
     console.log(`parts -> ${dateParts[2]}, ${dateParts[1]-1}, ${dateParts[0]}`)
@@ -5,10 +6,42 @@ export const subscriptionEndDate = (startDateStr) => {
     endDate.setDate(endDate.getDate() + 30)
     return endDate
 }
-
+//deprecated
 export const subscriptionRemain = (endDate) => {
     const currentDate = new Date()
     if (currentDate > endDate) return 0
     const daysRemain = Math.ceil((Math.abs(endDate - currentDate)) / (1000 * 3600 * 24))
     return daysRemain
+}
+/**
+ * 
+ * @param {*} expireDate date when subscription parish
+ * 
+ * return how many days is remain until sub ends
+ */
+export const subscriptionDaysRemain = (expireDate) => {
+    const [day, month, year] = expireDate.split('.').map(Number)
+    const targetDate = new Date(year, month-1, day)
+    
+    const today = new Date()
+    today.setHours(0,0,0,0)
+
+    const difference = targetDate - today
+    return Math.ceil(difference / (1000*3600*24))
+}
+export const subscriptionRemainPercent = (startDate, expireDate) => {
+    const daysRemains = subscriptionDaysRemain(expireDate)
+    const daysTotal = totalSubDays(startDate, expireDate)
+    return Math.round(daysRemains / daysTotal * 100)
+    
+}
+
+const totalSubDays = (startDateStr, expireDateStr) => {
+    const [expDay, expMonth, epxYear] = expireDateStr.split('.').map(Number)
+    const expireDate = new Date(epxYear, expMonth - 1, expDay)
+    const [startDay, startMonth, startYear] = startDateStr.split('.').map(Number)
+    const startDate = new Date(startYear, startMonth - 1, startDay)
+    const difference = expireDate - startDate
+    return Math.ceil(difference / (1000*3600*24))
+
 }
