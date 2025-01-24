8e72209eaa4c543b8771e9fe54e322e832f1fa7b
A1tBot <timofeyershovv@gmail.com>
2025-01-24 16:25:08 +0700

    Футер

--- src/components/footer_components/FooterMain.jsx
+++ src/components/footer_components/FooterMain.jsx
@@ -25,7 +25,7 @@ export const FooterMain = React.memo(({onScrollToSection}) => {
         {text: "Главная", func: () => createButtonFunc("section0")},
         {text: "Преимущества", func: () => createButtonFunc("section1")},
         {text: "Как начать учиться", func: () => createButtonFunc("section2")},
-        {text: "Партнеры", func: () => createButtonFunc("section4")},
+        {text: "Подписка", func: () => createButtonFunc("section3")},
         {text: "FAQ", func: () => createButtonFunc("section5")},
         {text: "Пополнение баланса", func: () => navigate(routes.PRICING)},
         {text: "Варианты", func: () => navigate(routes.TASK)},
