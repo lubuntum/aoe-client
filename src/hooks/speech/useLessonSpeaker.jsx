7c09296bec803d80d7d82772ac8f999bb17fe7d1
lubuntum <lubuntum@gmail.com>
2025-02-14 19:46:38 +0700

    Подключил новый text-to-speech сервис + немного подкорректировал старый, если что то случится с новым он заменит его.

--- src/hooks/speech/useLessonSpeaker.jsx
+++ src/hooks/speech/useLessonSpeaker.jsx
@@ -1,8 +1,21 @@
+
 export const useLessonSpeaker = () => {
-    
+    const findEnglishVoice = () => {
+        const synth = window.speechSynthesis
+        const voices = synth.getVoices()
+        return voices.find(voice => voice.lang === 'en-US' || voice.lang === 'en-GB')
+    }
     const speak = (text, onEndCallback) => {
+        if (window.responsiveVoice) {
+            window.responsiveVoice.speak(text, "UK English Female", {
+                onend: onEndCallback // Corrected from oneng to onend
+            })
+            return
+        }
         const speechTemp = new SpeechSynthesisUtterance(text)
         speechTemp.lang = "en-US"
+        const englishVoice = findEnglishVoice()
+        if (englishVoice) speechTemp.voice = englishVoice
 
         speechTemp.onend = () => {
             onEndCallback()
