896bdcb86be4f778f696d51fadc8025f5496f230
lubuntum <lubuntum@gmail.com>
2025-02-17 17:40:37 +0700

    Попытка исправить стоячий таймер

--- src/hooks/speech/useLessonSpeaker.jsx
+++ src/hooks/speech/useLessonSpeaker.jsx
@@ -1,5 +1,6 @@
 
 export const useLessonSpeaker = () => {
+    let isSpoken = false
     const findEnglishVoice = () => {
         const synth = window.speechSynthesis
         const voices = synth.getVoices()
@@ -9,11 +10,31 @@ export const useLessonSpeaker = () => {
         try {
             if (window.responsiveVoice) {
                 window.responsiveVoice.speak(text, "UK English Female", {
-                    onend: onEndCallback // Corrected from oneng to onend
+                    onend: () => {isSpoken = true; onEndCallback() }, // Corrected from oneng to onend
+                    onerror: (error) => {
+                        console.error("Error in response voice", error)
+                        speakDefault(text, onEndCallback)
+                    }
                 })
+            setTimeout(()=>{
+                if (window.speechSynthesis.speaking) {
+                    console.log("speaking...")
+                }
+                else if (!isSpoken) {
+                    console.warn("Speech did not start, falling back to default voice")
+                    speakDefault(text, onEndCallback)
+                }
+                else {
+                    console.log("is spoken: ", isSpoken)
+                }
+            }, 5000)
+            } else {
+                console.error("Cant connect to responsive voice")
+                speakDefault(text, onEndCallback)
             }
         } catch(err) {
             if (err !== undefined) console.error(err)
+            console.log("Some error occurred while using responsive voide API")
             speakDefault(text, onEndCallback)
         }
     }
