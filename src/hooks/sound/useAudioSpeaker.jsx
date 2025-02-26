ff6a570b19aa80670fe5fc93d6ac359277efee9c
lubuntum <lubuntum@gmail.com>
2025-02-26 16:10:57 +0700

    Спикер для воспроизведения аудио по url очередью

---
+++ src/hooks/sound/useAudioSpeaker.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useRef } from "react"
+
+export const useAudioSpeaker = () => {
+    const audioRef = useRef(null)
+    const speak = (url, onEnd) => {
+        if (!url){
+            onEnd()
+            return
+        }
+        if (audioRef.current) {
+            audioRef.current.pause()
+            audioRef.current.src = ""
+        }
+        const audio = new Audio(url)
+        audioRef.current = audio
+        audio.addEventListener("ended", () => cleanup(onEnd))
+        audio.play().catch(err=>{
+            console.error("Error while playing audio", err)
+            onEnd()
+        })
+    }
+    const cleanup = (onEnd) => {
+        if (!audioRef.current) {
+            onEnd()
+            return
+        }
+        audioRef.current.removeEventListener("ended", cleanup)
+        audioRef.current = null
+        onEnd()
+    }
+    return {speak}
+}
