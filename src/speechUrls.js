de72b3f9b3f3ad41499ca6eec904523d554f9bf1
lubuntum <lubuntum@gmail.com>
2025-02-26 16:11:16 +0700

    Набор url для дефолтных записей

---
+++ src/speechUrls.js
@@ -0,0 +1,4 @@
+export const speechUrls = {
+    bounce : "http://commondatastorage.googleapis.com/codeskulptor-assets/week7-brrring.m4a",
+    shoot : "http://codeskulptor-demos.commondatastorage.googleapis.com/GalaxyInvaders/player_shoot.wav"
+}
