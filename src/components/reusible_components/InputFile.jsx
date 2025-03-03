1fa2fa23d0ba1ec9e6794ddecb8ddcf2d94c7e6e
lubuntum <lubuntum@gmail.com>
2025-03-03 16:41:06 +0700

    Обновлен инпут файла, теперь можно задавать паттерн на формат файла

--- src/components/reusible_components/InputFile.jsx
+++ src/components/reusible_components/InputFile.jsx
@@ -3,12 +3,13 @@ import "./css/input_file.css"
 export const InputFile = ({inputFileName = "",
                            inputFileOnChange,
                            inputFileWidth = "",
-                           inputFileFor = ""}) => {
+                           inputFileFor = "",
+                           accept = null}) => {
     return (
         <div className="inputFileContainer"
              style={{minWidth: `${inputFileWidth}`, maxWidth: `${inputFileWidth}`}}>
             <label htmlFor={inputFileFor}><p>{inputFileName}</p></label>
-            <input type="file" name={inputFileFor} id={inputFileFor} onChange={inputFileOnChange}></input>
+            <input type="file" name={inputFileFor} id={inputFileFor} onChange={inputFileOnChange} accept={accept !== null && accept}></input>
         </div>
     )
 }
