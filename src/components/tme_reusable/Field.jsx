6df171b0be58bb81c0f63b87acd239f18e646151
A1tBot <timofeyershovv@gmail.com>
2026-01-04 18:47:23 +0700

    Добавил в поле input проверку если password то добавлять кнопку для видимости пароля

--- src/components/tme_reusable/Field.jsx
+++ src/components/tme_reusable/Field.jsx
@@ -1,7 +1,39 @@
 import "./css/inputs_style.css"
 
-export const Field = ({value, type = "text", onChange, ph}) => {
-    return (<>
-        <input className="input" type={type} value={value} onChange={onChange} placeholder={ph} required></input>
-    </>)
+import { useState } from "react"
+
+import { ReactComponent as VisibilityIcon } from "../../res/icons/visibility_24dp_gi.svg"
+import { ReactComponent as OffVisibilityIcon } from "../../res/icons/visibility_off_24dp_gi.svg"
+
+export const Field = ({ value, type = "text", onChange, onKeyPress, ph }) => {
+    const [showPassword, setShowPassword] = useState(false)
+
+    const isPasswordType = type === "password"
+
+    const inputType = isPasswordType && showPassword ? "text" : type
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword)
+    }
+
+    return (
+        <div className="field_container">
+            <input
+                className="input"
+                type={inputType}
+                value={value}
+                onChange={onChange}
+                onKeyDown={onKeyPress}
+                placeholder={ph}
+                required/>
+
+            {isPasswordType && (<>
+                {showPassword ? (
+                    <OffVisibilityIcon className="svg_icon" onClick={togglePasswordVisibility}/>
+                ) : (
+                    <VisibilityIcon className="svg_icon" onClick={togglePasswordVisibility}/>
+                )}
+            </>)}
+        </div>
+    )
 }
