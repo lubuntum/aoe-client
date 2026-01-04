34b527fc12e4f93b8ac704f26278c239ea75ca79
A1tBot <timofeyershovv@gmail.com>
2026-01-04 18:46:40 +0700

    Сделал новый loader компонент

--- src/components/tme_reusable/Loader.jsx
+++ src/components/tme_reusable/Loader.jsx
@@ -2,8 +2,13 @@ import "./css/loader_style.css"
 
 export const Loader = () => {
     return (<>
-        <div className="loader_container">
-            <div className="loader"></div>
+        {/*Loader copied from uiverse.io by dovatgabriel (Thanks)*/}
+        <div class="loader_container">
+            <div class="leap_loader">
+                <div class="leap_loader--dot"></div>
+                <div class="leap_loader--dot"></div>
+                <div class="leap_loader--dot"></div>
+            </div>
         </div>
     </>)
 }
